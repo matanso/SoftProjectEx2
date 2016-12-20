77fd121c77da57c03a7cebcaa28344e4c78a7c45
matan <matanso@gmail.com>
2016-12-20 15:13:29 +0200

    More tests for queue

--- unit_tests/sp_bpqueue_unit_tests.c
+++ unit_tests/sp_bpqueue_unit_tests.c
@@ -1,6 +1,5 @@
 #include "../SPBPriorityQueue.h"
 #include "unit_test_util.h"
-#include <stdbool.h>
 
 //Checks if copy Works
 bool queueBasicCopyTest() {
@@ -24,11 +23,30 @@ bool queueBasicCopyTest() {
     spBPQueueDestroy(p);
     return true;
 }
-bool pointBasicL2Distance() {
+bool queueBasicTest() {
+    int maxSize = 10;
+    double data[12] = {5, 4, 6, 3, 7, 2, 8, 1, 9, 0, 10, 3.5};
+    SPBPQueue *queue = spBPQueueCreate(maxSize);
+    for(double *val = data; val < data + 12; ++val) ASSERT_TRUE(spBPQueueEnqueue(queue, 0, *val) == SP_BPQUEUE_SUCCESS);
+    int a = spBPQueueGetMaxSize(queue);
+    ASSERT_TRUE(a == maxSize);
+    ASSERT_TRUE(spBPQueueSize(queue) == maxSize);
+    ASSERT_TRUE(spBPQueueIsFull(queue));
+    ASSERT_TRUE(!spBPQueueIsEmpty(queue));
+    ASSERT_TRUE(spBPQueueMaxValue(queue) == 8);
+    ASSERT_TRUE(spBPQueueMinValue(queue) == 0);
+    ASSERT_TRUE(spBPQueueDequeue(queue) == SP_BPQUEUE_SUCCESS);
+    ASSERT_TRUE(spBPQueueSize(queue) == maxSize - 1);
+    ASSERT_TRUE(!spBPQueueIsFull(queue));
+    ASSERT_TRUE(!spBPQueueIsEmpty(queue));
+    ASSERT_TRUE(spBPQueueMinValue(queue) == 1);
+    spBPQueueClear(queue);
+    ASSERT_TRUE(spBPQueueIsEmpty(queue));
+    spBPQueueDestroy(queue);
     return true;
 }
 int main() {
     RUN_TEST(queueBasicCopyTest);
-    RUN_TEST(pointBasicL2Distance);
+    RUN_TEST(queueBasicTest);
     return 0;
 }
